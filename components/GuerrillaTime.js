1bb29ea6195e66497e9501559af0700bcfc19dad
saito <saito@MacBook-Pro-13.local>
2019-12-23 18:52:58 +0900

    特攻キャラの追加

--- components/GuerrillaTime.js
+++ components/GuerrillaTime.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import moment from "moment";
 
 export class GuerrillaTime extends Component {
@@ -14,7 +14,8 @@ export class GuerrillaTime extends Component {
         "17:30",
         "20:30",
         "23:30"
-      ]
+      ],
+      date: "--:--"
     };
   }
 
@@ -22,45 +23,93 @@ export class GuerrillaTime extends Component {
     return 0 <= moment(moment().format("YYYY-MM-DD ") + time).diff(moment());
   };
 
+  isNow = time => {
+    return (
+      0 <=
+      moment(moment().format("YYYY-MM-DD ") + time).diff(
+        moment().subtract(30, "m")
+      )
+    );
+  };
+
   render() {
     return (
-      <View>
-        <View style={(styles.base_box, { flexDirection: "row" })}>
-          {this.state.guerrillaTime.map((time, index) => {
-            if (index <= 3) {
-              return (
-                <Text
-                  style={
-                    this.isFuture(time)
-                      ? styles.text_small_bold
-                      : styles.text_small_bold_unhilight
-                  }
-                  key={time}
-                >
-                  {time}
-                </Text>
-              );
-            }
-          })}
+      <View style={{ flexDirection: "row" }}>
+        <View style={{ flex: 5 }}>
+          <View style={[styles.base_box]}>
+            <View
+              style={[{ flexDirection: "row", width: 280, borderWidth: 0 }]}
+            >
+              {this.state.guerrillaTime.map((time, index) => {
+                if (index <= 3) {
+                  return (
+                    <Text
+                      style={[
+                        styles.guerrilla_time,
+                        this.isFuture(time)
+                          ? styles.text_small_bold
+                          : this.isNow(time)
+                          ? styles.text_small_bold_red
+                          : styles.text_small_bold_unhilight
+                      ]}
+                      key={time}
+                    >
+                      {time}
+                    </Text>
+                  );
+                }
+              })}
+            </View>
+
+            <View
+              style={[{ flexDirection: "row", width: 210, borderWidth: 0 }]}
+            >
+              {this.state.guerrillaTime.map((time, index) => {
+                if (index > 3) {
+                  return (
+                    <Text
+                      style={[
+                        styles.guerrilla_time,
+                        this.isFuture(time)
+                          ? styles.text_small_bold
+                          : this.isNow(time)
+                          ? styles.text_small_bold_red
+                          : styles.text_small_bold_unhilight
+                      ]}
+                      key={time}
+                    >
+                      {time}
+                    </Text>
+                  );
+                }
+              })}
+            </View>
+          </View>
+          <View
+            style={[
+              styles.base_box,
+              {
+                flexDirection: "row",
+                alignContent: "center",
+                width: 200,
+                margin: 5,
+                marginLeft: 40,
+                borderRadius: 30
+              }
+            ]}
+          >
+            <Text style={[styles.text_small_bold, { flex: 1 }]}>
+              次回開始まで
+            </Text>
+            <Text style={[styles.text_small_bold, { flex: 1 }]}>
+              {this.state.date}
+            </Text>
+          </View>
         </View>
-        <View style={(styles.base_box, { flexDirection: "row" })}>
-          {this.state.guerrillaTime.map((time, index) => {
-            if (index > 3) {
-              return (
-                <Text
-                  style={
-                    this.isFuture(time)
-                      ? styles.text_small_bold
-                      : styles.text_small_bold_unhilight
-                  }
-                  key={time}
-                >
-                  {time}
-                </Text>
-              );
-            }
-          })}
-        </View>
+        <Image
+          style={{ flex: 2, height: 75, resizeMode: "contain" }}
+          source={require("../assets/pinokio.png")}
+        />
       </View>
     );
   }
@@ -73,17 +122,25 @@ const styles = StyleSheet.create({
     paddingBottom: 10
   },
   text_small_bold: {
-    width: 75,
     fontSize: 15,
     color: "#140505",
     fontWeight: "bold",
     fontFamily: "Didot",
     textAlign: "center"
   },
+  guerrilla_time: {
+    flex: 1
+  },
   text_small_bold_unhilight: {
-    width: 75,
     fontSize: 15,
     color: "#707070",
+    fontWeight: "bold",
+    fontFamily: "Didot",
+    textAlign: "center"
+  },
+  text_small_bold_red: {
+    fontSize: 15,
+    color: "#9c0000",
     fontWeight: "bold",
     fontFamily: "Didot",
     textAlign: "center"
